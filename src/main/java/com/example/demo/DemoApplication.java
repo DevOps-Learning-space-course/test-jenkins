544f74d71d24822dbe75812e80c341d1048771e3
ZiyaadQasem <60221596+ZiyaadQasem@users.noreply.github.com>
2021-01-16 16:07:51 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String home() {
-		return "Hello World! v1.3";
+		return "Hello World! v1.0.0";
 	}
 
 	public static void main(String[] args) {
